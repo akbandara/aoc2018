e9dbc462a35bbeb0d988466046cbdaf62a1fdab0
Arosha K Bandara <akbandara@github.com>
2018-12-04 23:34:14 +0000

    Day 1 solved! - fixed input file path for common base folder

--- day1/duplicate-freq-finder.py
+++ day1/duplicate-freq-finder.py
@@ -2,7 +2,7 @@ freq = 0
 past_freq = {0}
 
 while True:
-    data = open('day1-input.txt', 'r').read().split('\n') # read the file
+    data = open('day1/day1-input.txt', 'r').read().split('\n') # read the file
     for line in data: # files are iterable
         freq = freq + int(line)
         #print("Adjusted Frequency by: " + str(int(line)) + " | New Freq = " + str(freq))
--- day1/frequency-calculator.py
+++ day1/frequency-calculator.py
@@ -1,6 +1,6 @@
 freq = 0
 
-data = open('day1-input.txt', 'r').read().split('\n') # read the file
+data = open('day1/day1-input.txt', 'r').read().split('\n') # read the file
 for line in data: # files are iterable
     freq = freq + int(line)
     print("Adjusted Frequency by: " + str(int(line)) + " | New Freq = " + str(freq))
