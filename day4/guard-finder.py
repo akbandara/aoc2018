ee6a0176683b0b2256f9346b6e143e4f9bfd51c6
Arosha K Bandara <akbandara@github.com>
2018-12-04 23:31:10 +0000

    Day 4 Solved! - tidy up code and remove logging

--- day4/guard-finder.py
+++ day4/guard-finder.py
@@ -8,17 +8,16 @@ log_entries.sort(key=lambda l: l.split(']')[0], reverse=False)
 
 for log_entry in log_entries:
     log_event = log_entry.split('] ')[1]
-    log_date = log_entry.split('] ')[0].split(' ')[0][6:11]
     log_minute = log_entry.split('] ')[0].split(' ')[1][3:5]
     if (log_event.startswith('Guard #')):
         guard_id = log_event[7:log_event.index(' ', 7)]
         if guard_id not in guard_log.keys():
             guard_log[guard_id] = {'Sleep Duration':0, 'Sleep Minutes':[0 for min in range(60)]}
-    if (log_event.startswith('falls')):
+    if (log_event.startswith('falls asleep')):
         sleep_time = int(log_minute)
-    if (log_event.startswith('wakes')):
+    if (log_event.startswith('wakes up')):
         wake_time = int(log_minute) 
-        print("Guard #%s fell asleep between %d and %d = %d mins" % (guard_id, sleep_time, wake_time, (wake_time-sleep_time)))
+        #print("Guard #%s fell asleep between %d and %d = %d mins" % (guard_id, sleep_time, wake_time, (wake_time-sleep_time)))
         guard_log[guard_id]['Sleep Duration'] = guard_log[guard_id]['Sleep Duration'] + (wake_time-sleep_time)
         for min in range(sleep_time, wake_time):
             guard_log[guard_id]['Sleep Minutes'][min] = guard_log[guard_id]['Sleep Minutes'][min] + 1
