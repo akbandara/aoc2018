65ec691fb715bb01bf4f6f3ce6d26c7ae2d96d83
Arosha K Bandara <akbandara@github.com>
2018-12-05 14:37:13 +0000

    Day 5 - Part 2 Solved!

--- day5/alchemical-reducer-fixed.py
+++ day5/alchemical-reducer-fixed.py
@@ -7,13 +7,17 @@ def remove_polymer_units(polymer_string, start_index):
 # read input
 base_polymer = open('day5/day5-input.txt', 'r').read()
 
+# variables for keeping track of the shortest polymer found
 shortest_polymer = len(base_polymer)
 fixed_unit = ''
 
+# iterate through the lower case ascii characters, each time removing the 'test_unit' from the base polymer
 for test_unit in ascii_lowercase:
     polymer = base_polymer.replace(test_unit, '').replace(test_unit.upper(), '')
     bar = Bar("Processing Reactions: Fixed %s" % test_unit, max=len(polymer))
     polymer_index=0
+    
+    # fully react the polymer be comparing adjacent pairs and removing if reaction condtions met
     while (polymer[polymer_index+1]!='\n'):
         if (str(polymer[polymer_index]).isupper() & str(polymer[polymer_index+1]).islower()):
             if (str(polymer[polymer_index])==str(polymer[polymer_index+1]).upper()):
@@ -31,6 +35,8 @@ for test_unit in ascii_lowercase:
             polymer_index = polymer_index + 1
         bar.next()
     bar.finish()
+
+    # check if resulting polymey is the shortest thus far and update variables if needed
     if (shortest_polymer > (len(polymer)-1)):
         shortest_polymer = len(polymer)-1
         fixed_unit = test_unit
