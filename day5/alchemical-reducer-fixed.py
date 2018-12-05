9582ad8d360b80c3c11642b3b4160e0ea9647089
Arosha K Bandara <akbandara@github.com>
2018-12-05 14:30:23 +0000

    Day 5 Solved!

---
+++ day5/alchemical-reducer-fixed.py
@@ -0,0 +1,37 @@
+from progress.bar import Bar
+from string import ascii_lowercase
+
+def remove_polymer_units(polymer_string, start_index):
+    return polymer_string[:start_index]+polymer[start_index+2:]
+
+# read input
+base_polymer = open('day5/day5-input.txt', 'r').read()
+
+shortest_polymer = len(base_polymer)
+fixed_unit = ''
+
+for test_unit in ascii_lowercase:
+    polymer = base_polymer.replace(test_unit, '').replace(test_unit.upper(), '')
+    bar = Bar("Processing Reactions: Fixed %s" % test_unit, max=len(polymer))
+    polymer_index=0
+    while (polymer[polymer_index+1]!='\n'):
+        if (str(polymer[polymer_index]).isupper() & str(polymer[polymer_index+1]).islower()):
+            if (str(polymer[polymer_index])==str(polymer[polymer_index+1]).upper()):
+                polymer = remove_polymer_units(polymer, polymer_index)
+                polymer_index = polymer_index-1
+            else:
+                polymer_index = polymer_index +1
+        elif (str(polymer[polymer_index]).islower() & str(polymer[polymer_index+1]).isupper()):
+            if (str(polymer[polymer_index])==str(polymer[polymer_index+1]).lower()):
+                polymer = remove_polymer_units(polymer, polymer_index)
+                polymer_index = polymer_index-1
+            else:
+                polymer_index = polymer_index +1
+        else:
+            polymer_index = polymer_index + 1
+        bar.next()
+    bar.finish()
+    if (shortest_polymer > (len(polymer)-1)):
+        shortest_polymer = len(polymer)-1
+        fixed_unit = test_unit
+        print("Shortest Polymer Length: %d | Unit = %s" % (shortest_polymer, fixed_unit))
